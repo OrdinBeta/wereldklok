ba8b886df84e706f9b067e7c30b3a1dcbf95ae3a
OrdinBeta <94678464+OrdinBeta@users.noreply.github.com>
2025-05-21 16:59:02 +0200

    feat: implement ClockService with HTTP methods for fetching clocks

--- Client/src/app/clock.service.ts
+++ Client/src/app/clock.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Clock } from './clock';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ClockService {
 
-  constructor() { }
+    private url = 'http://localhost:3010';
+
+    constructor(private http: HttpClient) { }
+
+    // $ is a convention for observables
+    getClocks$(): Observable<Clock[]> {
+        return this.http.get<Clock[]>(`${this.url}/my-clocks`);
+    }
+
+    // Using async/await
+    // Withoud rxjs observable
+    getClocks(): Promise<Clock[]> {
+        return firstValueFrom(this.http.get<Clock[]>(`${this.url}/my-clocks`));
+    }
 }
